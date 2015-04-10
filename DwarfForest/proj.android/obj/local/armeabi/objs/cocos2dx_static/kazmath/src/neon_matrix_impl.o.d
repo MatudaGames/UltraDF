11608860062084d31c178b2ad9169f3666727510
Kristaps <mtdr@inbox.lv>
2015-04-10 17:56:23 +0300

    delete

--- DwarfForest/proj.android/obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o: \
- /Users/kristapsauzins/Desktop/GameDev/2015/cocos2dx/cocos2d-x-2.2.6/projects/DwarfForest/proj.android/../../../cocos2dx/kazmath/src/neon_matrix_impl.c \
- /Users/kristapsauzins/Desktop/GameDev/2015/cocos2dx/cocos2d-x-2.2.6/projects/DwarfForest/proj.android/../../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h
-
-/Users/kristapsauzins/Desktop/GameDev/2015/cocos2dx/cocos2d-x-2.2.6/projects/DwarfForest/proj.android/../../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h:
--- DwarfForest/proj.android/obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o: \
- /Users/kristapsauzins/Desktop/GameDev/2015/cocos2dx/cocos2d-x-2.2.6/projects/DwarfForest/proj.android/../../../cocos2dx/support/ccUtils.cpp \
- /Users/kristapsauzins/Desktop/GameDev/2015/cocos2dx/cocos2d-x-2.2.6/projects/DwarfForest/proj.android/../../../cocos2dx/support/ccUtils.h
-
-/Users/kristapsauzins/Desktop/GameDev/2015/cocos2dx/cocos2d-x-2.2.6/projects/DwarfForest/proj.android/../../../cocos2dx/support/ccUtils.h:
